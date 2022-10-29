bc11e2bbb8302705c7c4b06c59c31d281dc8a952
Baribor <justluvia@gmail.com>
2022-10-29 22:13:13 +0100

    added github link to the github icon

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       <div className='socials flex gap-4 justify-center md:px-20 md:py-8'>
         <img src='/images/slack.png' alt='slack' className='w-7 h-7' />
-        <img src='/images/github.png' alt='slack' className='w-7 h-7' />
+        <img href="https://github.com/jussy562" src='/images/github.png' alt='slack' className='w-7 h-7'  />
       </div>
       <Footer />
     </div>
