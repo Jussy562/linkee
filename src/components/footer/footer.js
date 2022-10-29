be44dba3f0dcffa0caaa853301515bd6c53191a9
Baribor <justluvia@gmail.com>
2022-10-29 22:02:06 +0100

    Adjusted the spacing in the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='w-full md:px-20 md:py-8 md:pb-4 px-6 py-4'>
         <hr />
-        <div className='flex flex-col gap-8 md:flex-row md:justify-between md:items-center py-10'>
+        <div className='flex flex-col gap-2 md:gap-8 md:flex-row md:justify-between md:items-center py-10'>
             <img src="/images/zuri.Internship_Logo.png" alt='zuri internshinp' className='w-20 sm:h-auto' />
             <div className='flex'>
               <h3 className='font-inter text-gray-300'>HNG Internship 9 Frontend Task</h3>
